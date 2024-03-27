1c4a86d89fd4b44d79f085fd1bdf872a2aa2d4fb
Duron27 <duron27@gmail.com>
2024-03-27 12:48:19 -0400

    fix game works at emulators

--- app/src/main/java/com/arxlibertatis/presenter/SettingsFragmentPresenter.kt
+++ app/src/main/java/com/arxlibertatis/presenter/SettingsFragmentPresenter.kt
@@ -3,7 +3,10 @@ package com.arxlibertatis.presenter
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Environment
 import android.provider.DocumentsContract
+import android.util.Log
+import androidx.documentfile.provider.DocumentFile
 import com.arxlibertatis.interfaces.SettingsFragmentMvpView
 import com.arxlibertatis.ui.activity.ConfigureControlsActivity
 import com.arxlibertatis.utils.ASFUriHelper.getPath
@@ -22,19 +25,25 @@ class SettingsFragmentPresenter : MvpPresenter<SettingsFragmentMvpView>() {
     }
 
     fun saveGamePath(data: Intent, context: Context, preferences: SharedPreferences) {
-        val uri = data.data
-        val docUri = DocumentsContract.buildDocumentUriUsingTree(
-            uri,
-            DocumentsContract.getTreeDocumentId(uri)
-        )
 
-        val currentGamePath = getPath(context, docUri)
+        data?.data?.also { uri ->
+            val pattern = Regex("[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}")
+            val storageDir = Environment.getExternalStorageDirectory()
+            val storagePath = storageDir.absolutePath
+            val modifiedStoragePath = "/storage"
+            val pathSegment = uri.lastPathSegment
+            val currentGamePath = if (pattern.containsMatchIn(pathSegment ?: "")) {
+                modifiedStoragePath + "/" + pathSegment?.replace(":", "/")
+            } else {
+                storagePath + "/" + pathSegment?.replace("primary:", "")
+            }
 
-        with(preferences.edit()) {
-            putString(GAME_FILES_SHARED_PREFS_KEY, currentGamePath)
-            apply()
-            copyGameAssets(context, GAME_FILES_FOLDER_NAME, currentGamePath)
-            viewState?.updatePreference(GAME_FILES_SHARED_PREFS_KEY)
+            with(preferences.edit()) {
+                putString(GAME_FILES_SHARED_PREFS_KEY, currentGamePath)
+                apply()
+                copyGameAssets(context, GAME_FILES_FOLDER_NAME, currentGamePath)
+                viewState?.updatePreference(GAME_FILES_SHARED_PREFS_KEY)
+            }
         }
     }
 
