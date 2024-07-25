89b864425fa73851c6d845e49a5e1ab999431c34
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-07-25 20:39:45 +0400

    temporally use manage external storage

--- app/src/main/java/com/arxlibertatis/MainActivity.kt
+++ app/src/main/java/com/arxlibertatis/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.arxlibertatis
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,8 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.arxlibertatis.engine.startEngine
 import com.arxlibertatis.ui.theme.ArxLibertatisTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +36,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        requestPermission()
         startEngine(this)
+    }
+
+    private fun requestPermission() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val intent: Intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                intent.addCategory("android.intent.category.DEFAULT")
+                intent.setData(
+                    Uri.parse(
+                        String.format(
+                            "package:%s",
+                            applicationContext.packageName
+                        )
+                    )
+                )
+                startActivityForResult(intent, 2296)
+            } catch (e: Exception) {
+                val intent = Intent()
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+                startActivityForResult(intent, 2296)
+            }
+        } else {
+        }
     }
 }
 
