794df573f1d1a53f161d5a22b7a1e6fb7d1e8ceb
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-07-29 00:56:28 +0400

    use gl4es instead glew and epoxy for Android

--- app/src/main/java/com/arxlibertatis/engine/Engine.kt
+++ app/src/main/java/com/arxlibertatis/engine/Engine.kt
@@ -9,13 +9,14 @@ import com.arxlibertatis.utils.extensions.startActivity
 
 internal const val MAIN_ENGINE_NATIVE_LIB = "libarx.so"
 internal const val ARX_DATA_PATH_KEY = "ARX_DATA_PATH"
-internal val debugJniLibsArray= arrayOf( "SDL2","epoxy","freetyped","z","openal","arx")
-internal val jniLibsArray= arrayOf( "SDL2","epoxy","freetype","z","openal","arx")
+internal val debugJniLibsArray= arrayOf("GL", "SDL2","freetyped","z","openal","arx")
+internal val jniLibsArray= arrayOf("GL", "SDL2","freetype","z","openal","arx")
 internal val ARX_DATA_PATH = Environment.getExternalStorageDirectory().path + "/arx/"
 
 fun killEngine() = Process.killProcess(Process.myPid())
 
 fun startEngine(context: Context) {
+    Os.setenv("LIBGL_ES", "2", true)
     Os.setenv(ARX_DATA_PATH_KEY, ARX_DATA_PATH, true)
     context.startActivity<EngineActivity>()
 }
