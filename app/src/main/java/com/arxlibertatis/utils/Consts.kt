6f99a6af1f85c9622fddee387f512b050e43524a
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-08-21 18:25:49 +0400

    update privacy policy url

--- app/src/main/java/com/arxlibertatis/utils/Consts.kt
+++ app/src/main/java/com/arxlibertatis/utils/Consts.kt
@@ -1,6 +1,6 @@
 package com.arxlibertatis.utils
 
-const val PrivacyPolicyUrl = "https://github.com/sandstranger/ArxLibertatis-android-port/blob/huawei/PrivacyPolicy.txt"
+const val PrivacyPolicyUrl = "https://raw.githubusercontent.com/sandstranger/ArxLibertatis-android-port/huawei/PrivacyPolicy.txt"
 const val GAME_FILES_SHARED_PREFS_KEY = "game_files"
 const val CUSTOM_RESOLUTION_PREFS_KEY = "custom_resolution"
 const val GAME_FILES_FOLDER_NAME = "game_files"
